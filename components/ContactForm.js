65c932493c0e8cab03694a0565d48867880a2798
AakashKB <aakashkbalaji@gmail.com>
2018-10-08 20:53:26 -0400

    Form tests 1.12

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -35,7 +35,7 @@ export default class ContactForm extends Component {
                 className='contact-form' 
                 //onSubmit={this.handleSubmit} 
                 method='POST'
-                action='/'
+                action=''
                 onReset={this.handleReset}
                 data-netlify='true'
             >
