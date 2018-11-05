b077084a1f698978e7caeee5e6de5022aad8e31a
AakashKB <aakashkbalaji@gmail.com>
2018-11-05 10:14:21 -0500

    Fix mobile responsiveness

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,27 @@ import Intro from '../components/Intro';
 import Skills from '../components/Skills';
 import Activities from '../components/Activities';
 import ContactFooter from '../components/ContactFooter';
+import Head from 'next/head'
+
+/* TODO: 
+*   1. Check mobile responsiveness, althought it seems to work, it does not. Maybe worry about orientation?
+*   5. Fix Activity Card zoom
+*   2. Refactor CSS including browser optimization
+*   3. Add Favicon
+*   4. Optimize based on google insights
+*/
 
 const Index = (props) => (
     <div>
-        <Header/>
-        <Intro/>
-        <Skills/>
-        <Activities/>
-        <ContactFooter/>
+        <Head>
+            <title>Aakash Balaji</title>
+            <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
+        </Head>
+        <Header />
+        <Intro />
+        <Skills />
+        <Activities />
+        <ContactFooter />
         <style jsx global>{`
             @import url("https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700");
 
